76f1fee7d5e1b2c24afc2c3fb3fe0eaffd382f22
Marcos Souza <marcos13@outlook.com>
2020-03-06 00:13:40 -0300

    :sparkles: ChatStore Test

---
+++ test/app/stores/chat_store_test.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_modular/flutter_modular_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:chatway_atendente/app/app_module.dart';
+
+void main() {
+  initModule(AppModule());
+  // ChatStore chat;
+  //
+  setUp(() {
+    //     chat = AppModule.to.get<ChatStore>();
+  });
+
+  group('ChatStore Test', () {
+    //   test("First Test", () {
+    //     expect(chat, isInstanceOf<ChatStore>());
+    //   });
+
+    //   test("Set Value", () {
+    //     expect(chat.value, equals(0));
+    //     chat.increment();
+    //     expect(chat.value, equals(1));
+    //   });
+  });
+}
